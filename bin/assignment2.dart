a0af352f7e7aaf9a0a9f3dcc86de59f66ca45d2c
azamali4151 <azamalibd808@gmail.com>
2025-12-10 01:37:15 +0600

    problem 2 Employee class and two sub class

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -17,7 +17,37 @@ class Book{
     return discountedPrice;
   }
 }
+// Employee Class Start Here
+class Employee{
+
+  String ? name;
+  double ? salary;
+  Employee(this.name,this.salary);
+}
+class Manager extends Employee{
+
+  String ? department;
+  Manager(String name,double salary):super(name,salary);
+   completeInformation(String department){
+   print('Manager Name: ${name}');
+   print('Manager Department: ${department}');
+   print('Manager Salary: ${salary}');
+  }
+}
+class Developer  extends Employee{
+  Developer(String name,double salary):super(name,salary);
+  String ? programmingLanguage;
+
+  completeInformation(String programmingLanguage){
+    print('Developer Name: ${name}');
+    print('Programming Language: ${programmingLanguage}');
+    print('Developer Salary: ${salary}');
+  }
+}
+//Employee Class End Here
 void main(){
+  print('========= Problem 1 ===========');
+  print('\n');
   final newBook = Book('Agni Bina','Kazi Nazrul Islam',150);
   final newBook1 = Book('Seser Kobita','Rabindra Nath Thakur',150);
 
@@ -28,4 +58,13 @@ void main(){
 
   var disPrice1 =  newBook1.discountedPrice(10);
   print('Discounted Price: $disPrice1');
+  print('\n');
+  print('========= Problem 2 ===========');
+  print('\n');
+  Manager neEmployee = Manager('Md. Mahbubul Alam',50000);
+  neEmployee.completeInformation('It & MIS');
+  print('\n');
+  Developer neDeveloper = Developer('Md. Azam Ali',150000);
+  neDeveloper.completeInformation('Dart');
+
 }
